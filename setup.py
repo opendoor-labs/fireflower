f11f9803e1d80cd18273d2a8b0a12ea1afb95be2
Brian Bloniarz <brian.bloniarz@gmail.com>
2016-01-27 10:49:07 -0800

    Use version number from git tags.
    That'll be one less thing to update in sync for new releases.

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
+import subprocess
+
+version_num = subprocess.check_output(['git', 'describe', '--tags']).decode('ascii').rstrip()
 
 setup(
     name='fireflower',
-    version='1.0.4',
+    version=version_num,
     description='Enhancements for Luigi task management',
     url='https://github.com/opendoor-labs/fireflower',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
